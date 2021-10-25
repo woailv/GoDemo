6fe03f3e156c45bbc6afcd739ee87132fbeb50a0
woailv <2709988697@qq.com>
2021-10-25 16:55:40 +0800

    1

--- Temp/main.go
+++ Temp/main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	str := `[EnableVerboseS5]jcqwah8dnr all ping lost 
-hv updated successfully 1.2728m ago (2021-10-25 08:09:13.265633 +00)`
-	str = `[EnableVerboseS5]jcqwah8dnr all ping lost 
-hv updated successfully 1.5840m ago (2021-10-25 07:19:58.227921 +00)
-Network info:
-0.0.0.0/0 -> 192.168.1.1 wlan0`
-	fmt.Println(strings.Contains(str, "all ping lost"))
 }
 
 type T struct {
